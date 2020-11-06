bb43e320d4841423026042c6a2d8241bbac4a103
Mike Caine <mikey.caine@btinternet.com>
2020-11-06 15:39:11 +0000

    tidy up

--- src/main/java/com/mikeycaine/reactiveposts/client/Client.java
+++ src/main/java/com/mikeycaine/reactiveposts/client/Client.java
@@ -1,18 +1,15 @@
 package com.mikeycaine.reactiveposts.client;
 
-import com.mikeycaine.reactiveposts.client.content.PostsPageContent;
 import com.mikeycaine.reactiveposts.client.content.parsed.MainForumIndex;
 import com.mikeycaine.reactiveposts.client.content.parsed.PostsPage;
 import com.mikeycaine.reactiveposts.client.content.parsed.ThreadsIndex;
 import com.mikeycaine.reactiveposts.model.Forum;
-import com.mikeycaine.reactiveposts.model.Post;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import com.mikeycaine.reactiveposts.model.Thread;
 
 public interface Client {
-
     Mono<MainForumIndex> retrieveMainForumIndex();
 
     Mono<ThreadsIndex> retrieveThreads(Forum forum, int pageId);
@@ -21,8 +18,5 @@ public interface Client {
     Mono<Integer> latestPageId(Thread thread);
 
     Mono<PostsPage> retrievePosts(Thread thread, int pageId);
-
-    Flux<PostsPage>retrievePosts(Thread thread, int startPageId, int endPageId);
-
-
+    Flux<PostsPage> retrievePosts(Thread thread, int startPageId, int endPageId);
 }
