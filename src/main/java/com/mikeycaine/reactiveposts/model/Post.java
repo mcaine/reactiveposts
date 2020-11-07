90bc83a6f7fc2747be651cb211c0a1d33309c1ce
Mike Caine <mikey.caine@btinternet.com>
2020-11-07 14:12:35 +0000

    tidy

--- src/main/java/com/mikeycaine/reactiveposts/model/Post.java
+++ src/main/java/com/mikeycaine/reactiveposts/model/Post.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-
 import javax.persistence.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -15,27 +14,9 @@ import java.time.LocalDateTime;
 @Entity
 @ToString
 public class Post {
-//	public Post() {
-//
-//	}
-//
-//	// TODO Author here
-//	public Post(int postId, int pageNum, LocalDateTime postDate, Instant retrievedDate, Author author, String html, Thread thread) {
-//		this.id = postId;
-//		this.pageNum = pageNum;
-//		this.postDate = postDate;
-//		this.retrievedDate = retrievedDate;
-//		this.author = author;
-//		//this.authorName = authorName;
-//		//this.authorId = authorId;
-//		this.html = html;
-//		this.thread = thread;
-//	}
-
 	@Id
 	@Getter
 	@Setter
-	//@Column
 	@EqualsAndHashCode.Include
 	private Integer id;
 
@@ -62,14 +43,6 @@ public class Post {
 	@Column
 	@JsonSerialize(using= LocalDateTimeSerializer.class)
 	private Instant retrievedDate;
-
-//	@Getter @Setter
-//	@Column
-//	private String authorName;
-
-//	@Getter @Setter
-//	@Column
-//	private Integer authorId;
 
 	@Getter @Setter
 	@Column(columnDefinition = "TEXT")
