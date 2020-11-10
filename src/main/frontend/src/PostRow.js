3696fb548e9529056581c365c607fa2ab6b9d63f
Mike Caine <mikey.caine@btinternet.com>
2020-11-10 13:32:32 +0000

    PostRow.js

--- src/main/frontend/src/PostRow.js
+++ src/main/frontend/src/PostRow.js
@@ -16,7 +16,8 @@ class PostRow extends Component {
             <>
                 <tr>
 
-                    <td><div><h3>{post.author.name}</h3></div><div><Image alt={'icon'} src={post.author.titleURL}/></div></td>
+                    <td><div><h3>{post.author.name}</h3></div><div><img src={post.author.titleURL} alt="..." className="img-thumbnail"/></div></td>
+                    <td></td>
                     <td><span dangerouslySetInnerHTML={{__html: post.html}} /></td>
                     <td></td>
                     <td></td>
